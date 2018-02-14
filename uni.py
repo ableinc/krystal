59cd26063c8338431ea0bc3c7ec972ac263728a0
Able Inc <douglas.jaylen@gmail.com>
2018-02-13 19:21:35 -0500

    Update uni.py

--- uni.py
+++ uni.py
@@ -3,23 +3,24 @@ import os
 ROOT = os.path.dirname(os.path.abspath(__file__))
 KRYSTAL = os.path.join(ROOT, 'krystal.py')
 VERSION = '0901'
+UPDATEDUMP = os.path.join(ROOT, 'includes/')
 APIURL = 'https://ableinc.us/krystal/api/'
 UPDATEURL = 'https://ableinc.us/krystal/update/'
 IDENTPATH = os.path.join(ROOT, 'Identity.json')
 CONFIGJSON = os.path.join(ROOT, 'resources/userinfo.json')
 TRAINDATA = os.path.join(ROOT, 'model/test/traindata.json')
 TESTDATA = os.path.join(ROOT, 'model/test/testdata.json')
-KNOWNFACEFILES = ROOT + '/model/train'
-UNKNOWNFACEFILES = ROOT + '/model/test'
-SAVEDFACES = ROOT + '/model/faces.ai'
+KNOWNFACEFILES = os.path.join(ROOT, 'model/train')
+UNKNOWNFACEFILES = os.path.join(ROOT, 'model/test')
+SAVEDFACES = os.path.join(ROOT, 'model/faces.ai')
 DEFKEY = '23Able'
-MODEL = ROOT + '/model/Krystal.pmdl'
-DET_PROTOTXT = ROOT + '/model/MobileNetSSD.prototxt.txt'
-DET_MODEL = ROOT + '/model/MobileNetSSD.caffemodel'
+MODEL = os.path.join(ROOT, 'model/Krystal.pmdl')
+DET_PROTOTXT = os.path.join(ROOT, 'model/MobileNetSSD.prototxt.txt')
+DET_MODEL = os.path.join(ROOT, 'model/MobileNetSSD.caffemodel')
 
 # conversation
-PERSONMODEL = ROOT + '/conversation/krystal_beta_model'
-PERSONMODEL_TRAIN = ROOT + '/conversation/training_data'
-PERSONMODEL_JSON = ROOT + '/conversation/personality.json'
-PERSONMODEL_LOG = ROOT + '/conversation/tflearn_logs'
+PERSONMODEL = os.path.join(ROOT, 'conversation/krystal_beta_model')
+PERSONMODEL_TRAIN = os.path.join(ROOT, 'conversation/training_data')
+PERSONMODEL_JSON = os.path.join(ROOT, 'conversation/personality.json')
+PERSONMODEL_LOG = os.path.join(ROOT, 'conversation/tflearn_logs')
 
