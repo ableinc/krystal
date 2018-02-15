87360a588c890642bb9606cc8f1485b5d35b201a
Able Inc <douglas.jaylen@gmail.com>
2018-02-15 11:48:04 -0500

    Update depends.py

--- depends.py
+++ depends.py
@@ -1,2 +1,2 @@
 from setuptools.command.easy_install import main as install
-install(['tensorflow>=1.5', 'pyaudio', 'spacy', 'opencv-python', 'face_recognition', 'speechrecognition', 'tflearn', 'beautifulsoup', 'imutils'])
+install(['tensorflow', 'pyaudio', 'spacy', 'opencv-python', 'face_recognition', 'speechrecognition', 'tflearn', 'beautifulsoup', 'imutils'])
