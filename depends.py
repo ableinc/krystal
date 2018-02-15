02806bbb76cc04466cdd1801d1e76d0e7fd442b2
Able Inc <douglas.jaylen@gmail.com>
2018-02-15 15:13:07 -0500

    rewrote

--- depends.py
+++ depends.py
@@ -1,2 +1,10 @@
-from setuptools.command.easy_install import main as install
-install(['tensorflow', 'pyaudio', 'spacy', 'opencv-python', 'face_recognition', 'speechrecognition', 'tflearn', 'beautifulsoup', 'imutils'])
+import pip
+
+toinstall = ['tensorflow', 'pyaudio', 'spacy', 'opencv-python', 'face_recognition', 'speechrecognition', 'tflearn', 'beautifulsoup', 'imutils']
+
+def install(pkg):
+    pip.main(['install', pkg])
+
+if __name__ == '__main__':
+  for x in toinstall:
+      install(x)
